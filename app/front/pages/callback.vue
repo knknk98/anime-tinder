c8b64063fa9f1b13cfd28f98b9c18e24355e3d5a
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-03-17 15:21:24 +0900

    fix store

--- app/front/pages/callback.vue
+++ app/front/pages/callback.vue
@@ -8,5 +8,18 @@
 import axios from 'axios';
 
 export default {
+  async mounted() {
+    await axios.get('http://127.0.0.1:5000/user/callback', {
+      params: this.$route.query,
+    }).then(res => {
+      // set session
+      this.$store.commit('setAuthUser', res.data.sessionId);
+      this.$store.commit('setUserName', res.data.username);
+      this.$store.commit('setUserImage', res.data.profile_image_url);
+
+      this.$router.push('/');
+    }).catch(err => {
+    });
+  },
 };
 </script>
