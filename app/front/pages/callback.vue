e4d13fe96c895b7098a88eee4c5b0eff7823eacb
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-03-17 14:34:42 +0900

    res.dataのデバッグ

--- app/front/pages/callback.vue
+++ app/front/pages/callback.vue
@@ -9,19 +9,17 @@ import axios from 'axios';
 
 export default {
   async mounted() {
-    console.log(this.$route.query);
     await axios.get('http://127.0.0.1:5000/user/callback', {
       params: this.$route.query,
     }).then(res => {
-      console.log("success");
       // set session
+      console.log(res.data);
       this.$store.commit('setAuthUser', res.data.sessionId);
       this.$store.commit('setUserName', res.data.username);
       this.$store.commit('setUserImage', res.data.profile_image_url);
 
-      this.$router.push('/');
+      // this.$router.push('/');
     }).catch(err => {
-      console.log("failed");
     });
   },
 };
