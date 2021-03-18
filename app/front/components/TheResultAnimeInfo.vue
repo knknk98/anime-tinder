087f598f7837510dd103cb6158a9bc6266a5bc99
EC2 Default User <ec2-user@ip-10-0-0-65.ap-northeast-1.compute.internal>
2021-03-18 17:23:48 +0000

    アニメ画像：テキストの配置の比率を調整

--- app/front/components/TheResultAnimeInfo.vue
+++ app/front/components/TheResultAnimeInfo.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
       <v-container>
-          <v-row justify="space-between">
-              <v-col>
+          <v-row>
+              <v-col cols=3>
                   <v-img
                     src="https://www.lisani.jp/admin/wp-content/uploads/2021/01/2101101800-yh-001.jpg"
                     aspect-ratio=0.75
-                    contain
+                    max-height=400
+                    max-width=300
                     >
                   </v-img>
               </v-col>
-              <v-col>
-                  <v-card flat>BBB</v-card>
-                  aaaaaaaaaaaaaaaaaaaa
+              <v-col cols=9>
+                  <v-card>
+                      <v-card-text>
+                          貧乏な生活を送る高校２年生・上杉風太郎のもとに、好条件の家庭教師アルバイトの話が舞い込む。ところが教え子はなんと同級生！！　しかも五つ子だった！！全員美少女、だけど「落第寸前」「勉強嫌い」の問題児！最初の課題は姉妹からの信頼を勝ち取ること…！？毎日がお祭り騒ぎ！ 中野家の五つ子が贈る、かわいさ500％の五人五色ラブコメ開演！！
+                      </v-card-text>
+                  </v-card>
+                  #2021年春 #ハーレム #ラブコメ #学園
               </v-col>
           </v-row>
       </v-container>
--- app/front/components/TheResultTextBox.vue
+++ app/front/components/TheResultTextBox.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-card flat class="horizontal-gradient">
-    <div class="px-5 pb-1">
-      <div class="text-h6 white--text text-center font-weight-bold">診断結果</div>
-      <p class="text-h5 white black--text pa-5">
-          あなたにオススメのアニメは<strong class="red--text">hogehogehogehoge</strong>です！
-      </p>
-    </div>
-  </v-card>
+  <div class="ma-3">
+    <v-card flat class="horizontal-gradient">
+      <div class="px-5 pb-1">
+        <h1 class="white--text text-center font-weight-bold">診断結果</h1>
+        <p class="text-h5 white black--text pa-5">
+            あなたにオススメのアニメは<strong class="red--text">hogehogehogehoge</strong>です！
+        </p>
+      </div>
+    </v-card>
+  </div>
 </template>
