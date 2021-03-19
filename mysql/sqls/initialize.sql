a4fbf3de2c0ba57b39bd361fd149619e6e623df3
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-03-19 17:19:02 +0900

    database updated

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -3,26 +3,26 @@ USE anime_recommender
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `session_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `anime_data`;
 CREATE TABLE `anime_data` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `anime_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `image` blob,
+  `image` varchar(127),
   `description` text NOT NULL,
   `year` varchar(127) NOT NULL,
-  `genre` varchar(127) NOT NULL,
+  `genre` varchar(255) NOT NULL,
   `company` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  PRIMARY KEY (`anime_id`),
+  UNIQUE KEY `anime_id` (`anime_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `likeunlike`;
