8b42119dbac7f5dec26e9e6c09ea6e19b640a937
EC2 Default User <ec2-user@ip-10-0-0-65.ap-northeast-1.compute.internal>
2021-03-19 14:42:40 +0000

    .

--- app/front/.nuxt/components/plugin.js
+++ app/front/.nuxt/components/plugin.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 
 const components = {
   LoginButton: () => import('../../components/LoginButton.vue' /* webpackChunkName: "components/login-button" */).then(c => c.default || c),
-  NavSideBar: () => import('../../components/NavSideBar.vue' /* webpackChunkName: "components/nav-side-bar" */).then(c => c.default || c),
-  TheResultAnimeInfo: () => import('../../components/TheResultAnimeInfo.vue' /* webpackChunkName: "components/the-result-anime-info" */).then(c => c.default || c),
-  TheResultTextBox: () => import('../../components/TheResultTextBox.vue' /* webpackChunkName: "components/the-result-text-box" */).then(c => c.default || c)
+  NavSideBar: () => import('../../components/NavSideBar.vue' /* webpackChunkName: "components/nav-side-bar" */).then(c => c.default || c)
 }
 
 for (const name in components) {
