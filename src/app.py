5a5490afba6d1233c0a98e410c42e19b4ee80b53
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-03-21 00:34:58 +0900

    method modified

--- src/app.py
+++ src/app.py
@@ -177,7 +177,7 @@ def create_app():
             return redirect(ENV_VALUES['APP_URL'])
 
     # 指定した数(num)だけカードに表示するアニメの情報を取ってくる。DBにアクセスし、過去に表示したカード以外から適当に選んでくる。
-    # [GET] : "num": intに変換可能なstring, "sessionID": セッションID
+    # [POST] : {"num": intに変換可能なstring, "sessionID": string, "animeId": [int, ...]}
     # {
     #    'animes': [
     #        {
@@ -192,45 +192,50 @@ def create_app():
     #    ]
     # }
     # が返る
-    @app.route("/app/recs", methods=["GET"])
+    @app.route("/app/recs", methods=["POST"])
     def fetch_random_anime_data():
-        image_num = request.args.get("num")
-        session_id = request.args.get("sessionID")
-        # テストするときはパラメータが無いので他で適当にfilter
-        user = User.query.filter(User.session_id == session_id).first()
-        # image_num = "5"
-        # user = User.query.filter(User.name == "Kw_I_KU").first()
-        if user is not None:
-            lu_data = (
-                db.session.query(LikeUnlike)
-                .filter(LikeUnlike.user_id == user.user_id)
-                .all()
-            )
-            past_animes = [lu.anime_id for lu in lu_data]
-            # 過去に表示したことがあるものを含まないものからimage_num個に制限してとってくる
-            animes = (
-                db.session.query(AnimeData)
-                .filter(AnimeData.anime_id.notin_(past_animes))
-                .limit(int(image_num))
-                .all()
-            )
-            response_data = {
-                "animes": [
-                    {
-                        "id": anime.anime_id,
-                        "title": anime.title,
-                        "image": img_encode(anime.image),  # 画像をbase64で返す
-                        "description": anime.description,
-                        "year": anime.year,
-                        "genre": anime.genre.split(),
-                        "company": anime.company,
-                    }
-                    for anime in animes
-                ]
-            }
-            return jsonify(response_data)
-        else:
-            return redirect(ENV_VALUES['APP_URL'])
+        if request.method == "POST":
+            # テスト用クエリ（これをターミナルで打ち込むとpostでjsonが送信されます. データベースにも反映されるはずです）
+            # curl http://localhost:5000//app/recs -X POST -H "Content-Type: application/json" --data '{"num": "5", "sessionID": "value", "animeId": [4,7,8,9,10]}'
+            image_num = request.json['num']
+            session_id = request.json['sessionID']
+            anime_id_buffer = request.json['animeId']
+            # テストするときはパラメータが無いので他で適当にfilter
+            user = User.query.filter(User.session_id == session_id).first()
+            # image_num = "5"
+            # user = User.query.filter(User.name == "Kw_I_KU").first()
+            if user is not None:
+                lu_data = (
+                    db.session.query(LikeUnlike)
+                    .filter(LikeUnlike.user_id == user.user_id)
+                    .all()
+                )
+                past_animes = [lu.anime_id for lu in lu_data] + anime_id_buffer
+                past_animes = list(set(past_animes))
+                # 過去に表示したことがあるものを含まないものからimage_num個に制限してとってくる
+                animes = (
+                    db.session.query(AnimeData)
+                    .filter(AnimeData.anime_id.notin_(past_animes))
+                    .limit(int(image_num))
+                    .all()
+                )
+                response_data = {
+                    "animes": [
+                        {
+                            "id": anime.anime_id,
+                            "title": anime.title,
+                            "image": img_encode(anime.image),  # 画像をbase64で返す
+                            "description": anime.description,
+                            "year": anime.year,
+                            "genre": anime.genre.split(),
+                            "company": anime.company,
+                        }
+                        for anime in animes
+                    ]
+                }
+                return jsonify(response_data)
+            else:
+                return redirect(ENV_VALUES['APP_URL'])
 
     def user_anime_matrix():
         # user_idとanime_idを縦横にもち値がstatusの二次元配列を返す
