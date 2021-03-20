a196ba59879d6db1421a3c5fcfea69f1f0c8bdfa
EC2 Default User <ec2-user@ip-10-0-0-65.ap-northeast-1.compute.internal>
2021-03-20 16:59:10 +0000

    LIKEボタンとかにヘルプを実装

--- app/front/components/ResultButton.vue
+++ app/front/components/ResultButton.vue
@@ -1,15 +1,21 @@
 <template>
-  <v-btn
-    class="mx-2 button-orange"
-    fab
-    small
-    color="red"
-    @click="result"
-  >
-    <v-icon id="submit-gradient">
-      mdi-arrow-right-thick
-    </v-icon>
-  </v-btn>
+  <v-tooltip top>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        class="mx-2 button-orange"
+        fab
+        small
+        color="red"
+        @click="result"
+        v-bind="attrs" v-on="on"
+      >
+        <v-icon id="submit-gradient">
+          mdi-arrow-right-thick
+        </v-icon>
+      </v-btn>
+    </template>
+    <span>診断結果を見る</span>
+  </v-tooltip>
 </template>
 <script>
 export default {
